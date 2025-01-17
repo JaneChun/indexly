b601b47b06f7d69c74affb13b697b7ef044c3bf6
JaneChun <janechun22@gmail.com>
2025-01-17 20:05:38 +0900

    컬러셋 수정

--- constants/color.js
+++ constants/color.js
@@ -1,9 +1,12 @@
 export const Colors = {
 	daily: '#FF8383',
+	daily_opaque: 'rgba(255, 131, 131, 0.5)',
 	daily_light: '#FFEAEA',
 	weekly: '#FFB9B9',
+	weekly_opaque: 'rgba(255, 185, 185, 0.5)',
 	weekly_light: '#FFF5F5',
 	monthly: '#FFE2E2',
+	monthly_opaque: 'rgba(255, 226, 226, 0.5)',
 	monthly_light: '#FFFCFC',
 	done: '#e1e0df',
 	done_light: '#FCFCFC',
